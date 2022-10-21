12035d470d659086478fef5e340f87a441cc8c4a
Criki <1229089076@qq.com>
2022-10-22 04:39:38 +0800

    update network structure

--- network.py
+++ network.py
@@ -7,9 +7,10 @@ class TargetNetwork(nn.Module):
     def __init__(self, state_dim, action_dim, hidden_dim=64):
         super().__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, action_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
         t1 = F.tanh(self.fc1(x))
         t2 = F.tanh(self.fc2(t1))
-        return F.sigmoid(t2)
+        return F.sigmoid(self.fc3(t2))
